5089188e3cd21ba33899651282c081900e33c18c
KrouriB <brice.krouri@gmail.com>
2023-01-30 00:09:42 +0100

    refaire le git hub correctement

---
+++ index.php
@@ -0,0 +1,30 @@
+<h1>Exercice 1</h1>
+
+<p>I.Banque<br>
+<br><strong>Vous êtes chargé(e) de créer un projet orienté objet permettant de gérer des titulaires et leurs comptes bancaires respectifs.</strong><br>
+<br>Un compte bancaire est composé des éléments suivants :<br>
+<li>Un libellé (compte courant, livret A, ...)</li>
+<li>Un solde initial</li>
+<li>Une devise monétaire</li>
+<li>Un titulaire unique</li>
+<br>Un titulaire comporte :
+<li>Un nom</li>
+<li>Un prénom</li>
+<li>Une date de naissance</li>
+<li>Une ville</li>
+<li>L'ensemble de ses comptes bancaires.</li>
+<br>Sur un compte bancaire, on doit pouvoir :
+<li>Créditer le compte de X euros</li>
+<li>Débiter le compte de X euros</li>
+<li>Effectuer un virement d'un compte à l'autre.</li>
+<br>On doit pouvoir :
+<li>Affichertoutes  les  informations  d'un(e)  titulaire  (dont  l'âge)  et  l'ensemble  des  comptes appartenant à celui(celle)-ci.</li>
+<li>Afficher  toutes  les  informations  d'un  compte  bancaire,  notamment  le  nom  /  prénom  du titulaire du compte.</li></p>
+
+<h2>Résultat</h2>
+
+<?php
+
+
+
+?>
